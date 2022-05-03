19931131ca88e565d138a0402800a1daa26957ac
Kastuks <kkostass9@gmail.com>
2022-05-03 18:16:32 +0300

    Added shop page

--- src/screens/ShopScreen.js
+++ src/screens/ShopScreen.js
@@ -1,14 +1,107 @@
-import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import React, {useState, useEffect } from 'react';
+import {View, StyleSheet, Text, 
+	ScrollView, SafeAreaView, 
+	StatusBar, Image, ActivityIndicator} from 'react-native';
+
+import SearchBar from "./SearchBar";
+import List from "./List";
 
 const ShopScreen = () => {
+	const [searchPhrase, setSearchPhrase] = useState("");
+	const [clicked, setClicked] = useState(false);
+	const [fakeData, setFakeData] = useState();
+	useEffect(() => {
+		const getData = async () => {
+		  const apiResponse = await fetch(
+			"https://my-json-server.typicode.com/kevintomas1995/logRocket_searchBar/languages"
+		  );
+		  const data = await apiResponse.json();
+		  setFakeData(data);
+		};
+		getData();
+	  }, []);
 	return (
-		<View>
-			<Text>ShopScreen</Text>
-		</View>
+		<SafeAreaView style={styles.container}>
+			{!clicked && <Text style={styles.title}></Text>}
+      		<SearchBar
+        		searchPhrase={searchPhrase}
+        		setSearchPhrase={setSearchPhrase}
+        		clicked={clicked}
+        		setClicked={setClicked}
+      		/>
+          		{/* <List
+            		searchPhrase={searchPhrase}
+            		data={fakeData}
+            		setClicked={setClicked}
+          		/> */}
+			<ScrollView style={styles.scrollView}>
+				<Image source={require('../../pics/Flowers-shop.png')} style={styles.picStyle} />
+				<View style={styles.view}>
+				<Text style={styles.text}>Flower-shop-1</Text>
+					<Text>
+         				Flower shop description
+        			</Text>
+				</View>
+				<Text>{"\n"}</Text>
+				<Image source={require('../../pics/Flowers-shop1.png')} style={styles.picStyle} />
+				<View style={styles.view}>
+				<Text style={styles.text}>Flower-shop-2</Text>
+					<Text >
+         				Flower shop description
+        			</Text>
+				</View>
+				<Text>{"\n"}</Text>
+				<Image source={require('../../pics/Flowers-shop2.png')} style={styles.picStyle} />
+				<View style={styles.view}>
+				<Text style={styles.text}>Flower-shop-3</Text>
+					<Text >
+         				Flower shop description
+        			</Text>
+				</View>
+				<Text>{"\n"}</Text>
+				<Image source={require('../../pics/Flowers-shop3.png')} style={styles.picStyle} />
+				<View style={styles.view}>
+				<Text style={styles.text}>Flower-shop-4</Text>
+					<Text >
+         				Flower shop description
+        			</Text>
+				</View>
+			</ScrollView>
+		</SafeAreaView>
 	);
 };
 
-const styles = StyleSheet.create();
+// const styles = StyleSheet.create();
+
+const styles = StyleSheet.create({
+	container: {
+	  flex: 1,
+	},
+	scrollView: {
+	  marginHorizontal: 20,
+	},
+	picStyle: {
+		width: 350, 
+		height: 150,
+		marginLeft: 5,
+		borderRadius: 10,
+		borderWidth: 1,
+		borderColor: '#fff',
+	},
+	view: {
+		backgroundColor: 'rgba(144, 238, 144, 0.4)',
+		marginRight: 16,
+		marginLeft: 5,
+		paddingBottom: 20,
+		borderRadius: 10,
+		borderWidth: 1,
+		borderColor: '#fff',
+	},
+	text: {
+		color: 'mediumseagreen', 
+		opacity: 1,
+		fontSize: 22,
+	},
+  });
 
 export default ShopScreen;
