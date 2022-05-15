ef19c8d46ec4692a5296d73e0c04d225d1e4a1ed
Martynas Rafanavicius <Martynas.Raf@directmachining.com>
2022-05-15 14:16:54 +0300

    Navigation

--- App.js
+++ App.js
@@ -7,14 +7,32 @@ import MainScreen from './src/screens/MainScreen';
 import ShopScreen from './src/screens/ShopScreen';
 import CartScreen from './src/screens/CartScreen';
 import ProfileScreen from './src/screens/ProfileScreen';
+import ShopFrontScreen from './src/screens/ShopFrontScreen';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
+function HomeFlow() {
+	return (
+		<Stack.Navigator>
+			<Stack.Screen
+				name='MainFlow'
+				component={MainFlow}
+				options={{ headerShown: false }}
+			/>
+			<Stack.Screen name='ShopFront' component={ShopFrontScreen} />
+		</Stack.Navigator>
+	);
+}
+
 function MainFlow() {
 	return (
 		<Tab.Navigator>
-			<Tab.Screen name='Home' component={MainScreen} />
+			<Tab.Screen
+				name='Home'
+				component={MainScreen}
+				options={{ headerShown: false }}
+			/>
 			<Tab.Screen name='Shop' component={ShopScreen} />
 			<Tab.Screen name='Cart' component={CartScreen} />
 			<Tab.Screen name='About Me' component={ProfileScreen} />
@@ -25,16 +43,16 @@ function MainFlow() {
 export default function App() {
 	return (
 		<NavigationContainer>
-			<MainFlow />
+			<HomeFlow />
 		</NavigationContainer>
 	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
