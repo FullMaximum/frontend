5ddc4d6308e32239a46a062446876d8c0be80134
Martynas Rafanavicius <Martynas.Raf@directmachining.com>
2022-05-25 22:37:08 +0300

    Cart Screen init design

--- src/screens/CartScreen.js
+++ src/screens/CartScreen.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
+import FlowerCard from '../components/FlowerCard';
+import flowersApi from '../api/flowers';
+import CartFlowerCard from '../components/CartFlowerCard';
+import { Context as FlowerContext } from '../context/ShopContext';
 
 const CartScreen = () => {
+	const [flowers, setFlowers] = useState(null);
+
+	useEffect(async () => {
+		let response = await flowersApi.get('/getallflowers');
+
+		setFlowers(response.data.success ? response.data.returnObject : null);
+	}, []);
+
+	useEffect(() => {
+		console.log(flowers);
+	}, [flowers]);
+
 	return (
 		<View>
-			<Text>CartScreen</Text>
+			<CartFlowerCard />
 		</View>
 	);
 };
