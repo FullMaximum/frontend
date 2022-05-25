b943d9167d2fdb08a255c52f0208a7397ce171f7
Martynas Rafanavicius <Martynas.Raf@directmachining.com>
2022-05-26 02:13:13 +0300

    Cart Screen added container for ordered flowers

--- src/screens/CartScreen.js
+++ src/screens/CartScreen.js
@@ -1,5 +1,11 @@
 import React, { useState, useContext, useEffect } from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import {
+	View,
+	StyleSheet,
+	Text,
+	ScrollView,
+	TouchableOpacity,
+} from 'react-native';
 import FlowerCard from '../components/FlowerCard';
 import flowersApi from '../api/flowers';
 import CartFlowerCard from '../components/CartFlowerCard';
@@ -14,17 +20,48 @@ const CartScreen = () => {
 		setFlowers(response.data.success ? response.data.returnObject : null);
 	}, []);
 
-	useEffect(() => {
-		console.log(flowers);
-	}, [flowers]);
-
 	return (
-		<View>
-			<CartFlowerCard />
+		<View style={styles.container}>
+			<ScrollView style={styles.cartFlowersContainer}>
+				<CartFlowerCard />
+				<CartFlowerCard />
+				<CartFlowerCard />
+			</ScrollView>
+			<View style={styles.cartOverviewContainer}>
+				<Text style={styles.totalText}>total: 95€</Text>
+				<TouchableOpacity style={styles.orderNowButton}>
+					<Text style={styles.orderNowText}>Order Now!</Text>
+				</TouchableOpacity>
+			</View>
+			<View></View>
 		</View>
 	);
 };
 
-const styles = StyleSheet.create();
+const styles = StyleSheet.create({
+	container: {},
+	cartFlowersContainer: {
+		height: 330,
+	},
+	cartOverviewContainer: {},
+	totalText: {
+		textAlign: 'right',
+		marginHorizontal: 20,
+		fontSize: 19,
+	},
+	orderNowButton: {
+		backgroundColor: '#6C9673',
+		width: '60%',
+		height: 40,
+		borderRadius: 20,
+		alignSelf: 'center',
+		marginVertical: 10,
+	},
+	orderNowText: {
+		color: 'white',
+		fontSize: 24,
+		textAlign: 'center',
+	},
+});
 
 export default CartScreen;
